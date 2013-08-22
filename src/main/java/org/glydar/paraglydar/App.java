0221fc795342beb5551990f880924dd4bfa1e240
Daniel Shnell <theultimatebudgie@gmail.com>
2013-08-22 18:48:09 +1000

    Initial mavenising

---
+++ src/main/java/org/glydar/paraglydar/App.java
@@ -0,0 +1,13 @@
+package org.glydar.paraglydar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
