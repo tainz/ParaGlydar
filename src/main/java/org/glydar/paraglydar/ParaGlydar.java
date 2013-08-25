599910f94ae87e949db28d4cd1125a7ec6be3261
Evan Lindsay <evanlindsay@live.com>
2013-08-25 05:33:41 -0700

    Start working on logger implementation.

--- src/main/java/org/glydar/paraglydar/ParaGlydar.java
+++ src/main/java/org/glydar/paraglydar/ParaGlydar.java
@@ -1,7 +1,5 @@
 package org.glydar.paraglydar;
 
-import org.glydar.paraglydar.util.LogFormatter;
-
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
@@ -11,30 +9,21 @@ import java.util.logging.Logger;
  */
 public class ParaGlydar
 {
-	private static final Logger LOGGER = Logger.getLogger(ParaGlydar.class.getName());
 	private static Server s;
 
-	private ParaGlydar() {
-		LOGGER.setUseParentHandlers(false);
-		LogFormatter format = new LogFormatter();
-		ConsoleHandler console = new ConsoleHandler();
-		console.setFormatter(format);
-		LOGGER.addHandler(console);
-	}
-
-    public static void setServer(Server server){
-    	if (s == null){
+    public static void setServer(Server server) {
+    	if (s == null) {
     		s = server;
     	} else {
-    		s.getLogger().severe("Can't change the server instance!");
+    		getLogger().severe("Can't change the server instance!");
     	}
     }
     
-    public static Server getServer(){
+    public static Server getServer() {
     	return s;
     }
 
 	public static Logger getLogger() {
-		return LOGGER;
+		return s.getLogger();
 	}
 }
