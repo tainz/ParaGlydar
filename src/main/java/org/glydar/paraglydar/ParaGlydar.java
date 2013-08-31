c226fa24dbdf7482690d52a435a4a288d5a91a52
Daniel Shnell <theultimatebudgie@gmail.com>
2013-08-31 13:33:35 +1000

    derp...

--- src/main/java/org/glydar/paraglydar/ParaGlydar.java
+++ src/main/java/org/glydar/paraglydar/ParaGlydar.java
@@ -66,7 +66,7 @@ public final class ParaGlydar {
 	}
 	
 	public static void setPluginLoader(PluginLoader pluginLoader) {
-		if (s == null) {
+		if (pl == null) {
 			pl = pluginLoader;
 		} else {
 			getLogger().severe("Can't change the plugin loader instance!");
