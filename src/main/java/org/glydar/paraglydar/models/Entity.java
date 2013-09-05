623a3b5bec2c8c9071a891aa8e68b9a39cff05d2
Daniel Shnell <theultimatebudgie@gmail.com>
2013-09-05 12:38:46 +1000

    heal/harm/damage/stun

--- src/main/java/org/glydar/paraglydar/models/Entity.java
+++ src/main/java/org/glydar/paraglydar/models/Entity.java
@@ -20,6 +20,18 @@ public interface Entity {
 	public EntityData getEntityData();
 
 	public void setEntityData(EntityData ed);
+	
+	public void sendMessageFromEntity(BaseTarget target, String message);
+	
+	public void sendMessageFromEntity(String message);
+	
+	public void damage(float damage);
+	
+	public void kill();
+	
+	public void setHealth(float health);
+	
+	public void heal(float health);
 
 	public boolean equals(Object o);
 
