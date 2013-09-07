fc05ee2258c0aec8ade2d2259cfea889a4170f06
aumgn <aumgn@free.fr>
2013-09-07 21:11:28 +0200

    Add set* methods to Orientation

--- src/main/java/org/glydar/paraglydar/geom/Orientation.java
+++ src/main/java/org/glydar/paraglydar/geom/Orientation.java
@@ -23,4 +23,28 @@ public class Orientation {
 	public float getYaw() {
 		return yaw;
 	}
+
+	public Orientation setRoll(float newRoll) {
+		return new Orientation(newRoll, pitch, yaw);
+	}
+
+	public Orientation setPitch(float newPitch) {
+		return new Orientation(roll, newPitch, yaw);
+	}
+
+	public Orientation setYaw(float newYaw) {
+		return new Orientation(roll, pitch, newYaw);
+	}
+
+	public Orientation setRollAndPitch(float newRoll, float newPitch) {
+		return new Orientation(newRoll, newPitch, yaw);
+	}
+
+	public Orientation setRollAndYaw(float newRoll, float newYaw) {
+		return new Orientation(newRoll, pitch, newYaw);
+	}
+
+	public Orientation setPitchAndYaw(float newPitch, float newYaw) {
+		return new Orientation(roll, newPitch, newYaw);
+	}
 }
